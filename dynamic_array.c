ba5631ad072e51047bad02a8676624d4d305aa3f
ljz <hdifhd@qq.com>
2022-02-08 18:29:09 +0800

    Add dynamic array module.

---
+++ dynamic_array.c
@@ -0,0 +1,67 @@
+#include <string.h>
+#include "dynamic_array.h"
+
+const size_t initial_size = 16 * elem_size;  // inital 16 element
+const size_t size_of_element = elem_size;
+
+dynamic_array* new_dynamic_array()
+{
+    dynamic_array* array = malloc(sizeof(dynamic_array));
+    array->size = initial_size;
+    array->array = malloc(initial_size);
+    array->length = 0;
+    
+    return array;
+}
+
+
+void destroy_dynamic_array(dynamic_array* d_array)
+{
+    free(d_array->array);
+    d_array->array = NULL;
+    free(d_array);
+}
+
+
+void push(dynamic_array* d_array, element_t value)
+{
+    if(d_array->length * sizeof(element_t) == d_array->size)
+    {
+        d_array->size <<= 1;
+
+        element_t* old_array = d_array->array;
+        element_t* new_array = malloc(d_array->size);
+
+        memcpy
+        (
+            new_array, 
+            old_array, 
+            d_array->length * sizeof(element_t)
+        );
+
+        free(old_array);
+        d_array->array = new_array;
+    }
+    
+    d_array->array[d_array->length] = value;
+    d_array->length++;
+}
+
+
+void print_dynamic_array(dynamic_array* d_array)
+{
+    size_t i;
+    for(i = 0; i < d_array->length; i++)
+        printf(", %llu" + ((!i) * 2), d_array->array[i]);
+    
+    putchar('\n');
+}
+
+
+void to_file(dynamic_array* d_array, FILE* fp)
+{
+    fwrite(&d_array->length, sizeof(size_t), 1, fp);   // 第一个 8 字节保存数组长度
+    fwrite(&size_of_element, sizeof(size_t), 1, fp);   // 第一个 8 字节保存元素大小
+
+    fwrite(d_array->array, sizeof(element_t), d_array->length, fp);
+}
---
+++ dynamic_array.h
@@ -0,0 +1,42 @@
+#ifndef DYNAMIC_ARRAY_HDIFHD
+#define DYNAMIC_ARRAY_HDIFHD
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long element_t;   // 以后要改就改这里，这里是元素类型
+#define elem_size sizeof(element_t)
+
+typedef struct dy_array
+{
+    element_t* array;
+    size_t length;
+    size_t size;
+}dynamic_array;
+
+/* 生成一个空的动态扩容数组 */
+dynamic_array* new_dynamic_array();
+
+/* 删除动态扩容数组 */
+void destroy_dynamic_array(dynamic_array* d_array);
+
+void push(dynamic_array* d_array, element_t value);
+
+/* 
+    打印动态扩容数组
+    以元素类型为 unsigned long long 来打印
+    2022-2-8 18:16:43
+ */
+void print_dynamic_array(dynamic_array* d_array);
+
+/* 
+    输出到文件
+    以
+        length (8 Bytes)
+        elem_size (8 Bytes)
+        data..
+    保存
+ */
+void to_file(dynamic_array* d_array, FILE* fp);
+
+#endif
