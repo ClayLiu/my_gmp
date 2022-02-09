c5ca56b6118e8a86b727422e0bbedd8dcf1f5890
ljz <hdifhd@qq.com>
2022-02-09 12:33:32 +0800

    Done printing in binary way.

---
+++ my_gmp.c
@@ -0,0 +1,77 @@
+#include "my_gmp.h"
+
+
+huge_number* new_huge_number(unsigned long long x, bool sign)
+{
+    huge_number* hn = malloc(sizeof(huge_number));
+    
+    hn->da = new_dynamic_array();
+    hn->sign = sign;
+
+    da_push(hn->da, x);
+    return hn;
+}
+
+
+void destroy_huge_number(huge_number* hn)
+{
+    destroy_dynamic_array(hn->da);
+    free(hn);
+}
+
+
+huge_number* new_huge_number_one()
+{
+    return new_huge_number(1, positive_sign);
+}
+
+
+huge_number* new_huge_number_zero()
+{
+    return new_huge_number(0, positive_sign);
+}
+
+
+void print_huge_number_bin(const huge_number* hn)
+{
+    char j;
+    size_t i;
+    unsigned long long number;
+    const unsigned long long kernel = (unsigned long long)1 << 31;
+
+    const size_t length = hn->da->length;
+    const unsigned long long* array = hn->da->array;
+
+    if(hn->sign == negative_sign)
+        putchar('-');
+    
+    for(i = 0; i < length; i++)
+    {
+        printf("," + (!i));
+        number = array[length - 1 - i];
+        // printf("%llu\n", number);
+
+        for(j = 0; j < 32; j++)
+        {
+            if(number & kernel)
+                putchar('1');
+            else
+                putchar('0');
+            
+            number <<= 1;
+        }
+    }
+}
+
+
+/* l += r */
+void add_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+/* l -= r */
+void sub_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+/* l *= r */
+void mul_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+/* l /= r */
+void div_inplace(huge_number* hn_l, const huge_number* hn_r);
---
+++ my_gmp.h
@@ -0,0 +1,53 @@
+#ifndef MY_GMP_H_HDIFHD
+#define MY_GMP_H_HDIFHD
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dynamic_array.h"
+
+typedef char bool;
+
+enum sign_value {positive_sign = 0, negative_sign = 1};
+
+typedef struct hn
+{
+    dynamic_array* da;
+    bool sign;  // negative -> 1; positive -> 0;
+}huge_number;
+
+/* 生成一个超大数 */
+huge_number* new_huge_number(unsigned long long x, bool sign);
+
+/* 删除超大数 */
+void destroy_huge_number(huge_number* hn);
+
+/* 超大数 1 */
+huge_number* new_huge_number_one();
+
+/* 超大数 0 */
+huge_number* new_huge_number_zero();
+
+/* 输出 */
+
+    /* 以二进制打印超大数 */
+    void print_huge_number_bin(const huge_number* hn);
+
+    /* 以十进制打印超大数 */
+    void print_huge_number_dec(const huge_number* hn);
+
+
+/* 原地四则运算 */
+
+    /* l += r */
+    void add_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+    /* l -= r */
+    void sub_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+    /* l *= r */
+    void mul_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+    /* l /= r */
+    void div_inplace(huge_number* hn_l, const huge_number* hn_r);
+
+#endif
